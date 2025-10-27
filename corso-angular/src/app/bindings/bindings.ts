3d238d241d7efec80dec193c405011c5e8b5ee01
NazariyTeslya <nazariy.teslya@marconirovereto.it>
2025-10-27 11:05:20 +0000

    Refactor code structure for improved readability and maintainability

--- corso-angular/src/app/bindings/bindings.ts
+++ corso-angular/src/app/bindings/bindings.ts
@@ -10,7 +10,19 @@ export class Bindings {
   // crea un signal se si aggiorna alla modifica
   studente: WritableSignal<string> = signal("Federico");
 
+  // signal per un contatore numerico
+  contatore: WritableSignal<number> = signal(0);
+  immagini: string []= ['corso-angular/public/500.png', 'corso-angular/public/aereo.png', 'corso-angular/public/scooter.png'];
+  indiceImmagine: WritableSignal<number> = signal(0);
   cambiaNome(): void {
     this.studente.set("Luca Boschi");
+  }
+
+  incrementa(): void {
+    this.contatore.update(n => n + 1);
+  }
+
+  decrementa(): void {
+    this.contatore.update(n => n - 1);
   }
 }
